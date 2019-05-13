d652d9e832da5bfa9a97ff7db96f0a520393f64b
clarencecarpenter <c.anthony.ca354@gmail.com>
2019-05-13 13:18:07 -0500

    Initial Wizpic set-up

---
+++ src/main/java/org/launchcode/wizpic/WizpicApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.wizpic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizpicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizpicApplication.class, args);
+	}
+
+}
